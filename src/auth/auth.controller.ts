339bcf8776d58004c246452761a6517644acd7e9
poseidon <jsy10303@gmail.com>
2024-06-22 15:47:19 +0900

    섹션13 토큰시스템 사용토록 엔드포인트 변경하기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,11 +6,21 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login/email')
-  loginEmail(@Body('email') email: string, @Body('password') password: string) {
-    return this.authService.loginWithEmail({
-      email,
-      password,
-    });
+  loginEmail(@Headers('authorization') rawToken: string) {
+    // loginEmail(@Body('email') email: string, @Body('password') password: string) {
+    // email:password 가 Base 64로 변형돼 있다
+    // 즉, alddlcodlk3ldlslscsldldl 식으로
+    // 이것을 email:password 로 변형 시키고,
+    // 다시 : 기준으로 분리해 사용한다.
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodeBasicToken(token);
+
+    return this.authService.loginWithEmail(credentials);
+    // return this.authService.loginWithEmail({
+    //   email,
+    //   password,
+    // });
   }
 
   @Post('register/email')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,6 +11,66 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
   ) {}
+
+  /**
+   * 토큰을 사용하게 되는 방식
+   *  1. 사용자가 로그인 또는 회원가입을 진행하면
+   *    accessToken과 refreshToken을 발급받는다
+   *  2. 로그인 할 때는 Basic 토큰과 함께 요청을 보낸다.
+   *    Basic 토큰은 '이메일:비밀번호'를 Base64로 인코딩한 형태이다
+   *    예) {authorization:'Basic {token}}
+   *  3.아무나 접근할 수 없는 정보(private route)를 접근할 때는
+   *    accessToken을 Header에 추가해서 요청과 함께 보낸다.
+   *    예) {authorization:'Bearer {token}}
+   *  4. 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을
+   *    보낸 사용자가 누구인지 알 수 있다.
+   *    예) 현재 로그인한 사용자가 작성한 포스트만 가져오려면 토큰의
+   *      sub 값에 입력돼 있는 사용자의 포스트만 따로 필터링 할 수 있다.
+   *      특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근하지 못한다.
+   *  5. 모든 토큰은 만료 기간이 있다. 만료 기간이 지나면 새로 토큰을 발급
+   *      받아야 한다. 그렇지 않으면 jstService.verify()에서 인증이 통과 되지 못한다.
+   *      따라서 access 토큰을 새로 발급 받을 수 있는 /auth/token/access와
+   *      refresh 토큰을 받을 수 있는 /auth/token/refresh가 필요하다
+   *  6. 토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 위의 엔드포인트
+   *      요청을 통해 새로운 토큰을 발급받고 새로운 토큰을 이용해 private route에
+   *      접근한다.
+   */
+
+  /**
+   * Header로 부터 토큰을 받을 때
+   * {authorization:'Basic {token}'}
+   * {authorization:'Bearer {token}'}
+   *
+   */
+
+  extractTokenFromHeader(header: string, isBearer: boolean) {
+    const splitToken = header.split(' ');
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+    if (splitToken.length !== 2 || prefix !== splitToken[0]) {
+      throw new UnauthorizedException('잘못 된 토큰');
+    }
+
+    const token = splitToken[1];
+    return token;
+  }
+
+  // 즉, alddlcodlk3ldlslscsldldl 식으로
+  // 이것을 email:password 로 변형 시키고,
+  // 다시 : 기준으로 분리해 사용한다.
+  decodeBasicToken(base64String: string) {
+    const decoded = Buffer.from(base64String, 'base64').toString('utf8');
+    const split = decoded.split(':');
+    if (split.length !== 2) {
+      throw new UnauthorizedException('잘못 된 유형의 토큰');
+    }
+    const email = split[0];
+    const password = split[1];
+    return {
+      email,
+      password,
+    };
+  }
+
   /**
    * 만드는 기능
    * 1. registerWithEmail
