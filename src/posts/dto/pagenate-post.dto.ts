9c258596df7b2a247f59d16baa6786d6673d92fb
poseidon <jsy10303@gmail.com>
2024-06-25 15:13:34 +0900

    섹션22 typeAnotation과 Impllicit Conversion 적용해 보기

--- src/posts/dto/pagenate-post.dto.ts
+++ src/posts/dto/pagenate-post.dto.ts
@@ -1,6 +1,8 @@
+// import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
 export class PaginatePostDto {
+  // @Type(() => Number)
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
